a6dabe0f011467a4f697bd09ae9901b55d5d489e
Liang-ZX <3170105151@zju.edu.cn>
2023-02-26 22:48:21 +0800

    Update download_test.sh

--- src/data/download_test.sh
+++ src/data/download_test.sh
@@ -7,7 +7,7 @@ fileid='1-I1Rp1VrF4S5hx9_X3MUL50C10Ph1Tqe'
 # Download zip dataset from Google Drive
 filename='test.zip'
 
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
+wget --wait 10 --random-wait --continue --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
 
 # Unzip
 unzip ${filename}
